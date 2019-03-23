b3fbf4a2301063c4df0913ca378afae4804de5dd
shiva <you@example.com>
2019-03-23 15:06:04 +0530

    scorepage,bowlerpage,commmentry_page modifed

--- scorepage/views.py
+++ scorepage/views.py
@@ -4,6 +4,15 @@ from home_app.views import c
 
 def score(request, id):
    score = c.scorecard(id)
-   send = {'data':score}
-   print(score['scorecard'])
+   info = c.matchinfo(id)
+   commentary = c.commentary(id)
+   send = {
+            'team1':score['scorecard'][0],
+            'team2':score['scorecard'][1],
+            'matchinfo': info,
+            'commentary':commentary,
+            'bowlcard_1':score['scorecard'][0]['bowlcard']
+            }
+
+   print()
    return render(request, 'score/score.html', send)
