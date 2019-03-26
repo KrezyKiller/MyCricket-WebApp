a8f26fbdcc04a0fe754e332f3d7e8551fd019bab
shiva <you@example.com>
2019-03-26 14:51:07 +0530

    scorepage modified

--- scorepage/views.py
+++ scorepage/views.py
@@ -6,19 +6,19 @@ def score(request, id):
    score = c.scorecard(id)
    info = c.matchinfo(id)
    commentary = c.commentary(id)
-   print(c.matchinfo(id))
-   send = {      'team1':score['scorecard'][0],
-                 'bowlcard_1':score['scorecard'][0]['bowlcard'],
+   print(commentary)
+   send = {
                  'matchinfo': info,
-                 'commentary':commentary,
+                 'commentary':commentary['commentary'],
                  }
    if info['mchstate'] == 'mom' or 'inprogress' or 'innings break':
       try:
+          send['status'] = 'Ok'
+          send['team1'] = score['scorecard'][0]
+          send['bowlcard_1'] = score['scorecard'][0]['bowlcard'],
           send['team2'] = score['scorecard'][1]
           send['bowlcard_2'] = score['scorecard'][1]['bowlcard']
-
       except:
           send['team2'] = ['None']
-
-
+          send['status'] = 'Preview'
    return render(request, 'score/score.html',send)
